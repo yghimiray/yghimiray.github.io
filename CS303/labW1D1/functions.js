e2a7b59b31952bb3068fe645bc756a2a332ef645
Yogesh <yghimiray@miu.edu>
2021-03-22 19:15:08 -0500

    aa

---
+++ CS303/labW1D1/functions.js
@@ -0,0 +1,58 @@
+"use strict";
+
+
+
+/**
+ * 
+ * @param {number} a is a number
+ * @param {number} b is a number 
+ * @param {number} c is a number
+ * @returns {number} largest of a, b, c
+ */
+function maxOfThree(a, b, c){ 
+    return c;
+}
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function sum(arr){
+    let tot = 0;
+
+    return tot;
+}
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function multiply(arr){
+    let tot = 1;
+
+    return tot;
+
+}
+
+// const matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+// function sumMatrix (arr){
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         for (let j = 0; j < arr[i].length; j++){
+//             sum = sum + arr[i][j];
+//         }
+//     }
+//     return sum;
+// }
+
+// function sumArray(arr){
+//     let sum = 0;
+//     for(const outElem of arr){
+//         for(const inElem of outElem ){
+//             sum + = inElem;
+//         }
+//     }
+// }
