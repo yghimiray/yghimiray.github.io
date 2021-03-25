01c3b904e7b5df53c996dbccfe240b9bf455650c
Yogesh <yghimiray@miu.edu>
2021-03-24 21:55:12 -0500

    a

--- CS303/labW1D3/app.js
+++ CS303/labW1D3/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = { Accumulator, Calculator }; //add all of your function names here that you need for the node mocha tests
+// module.exports = { Accumulator, Calculator }; //add all of your function names here that you need for the node mocha tests
 
 /**
  * @param {number} initialValue an initial number.
