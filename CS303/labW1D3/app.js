7188df4326247957f14ba2adb06068e3f2554806
Yogesh <yghimiray@miu.edu>
2021-03-24 21:30:03 -0500

    Update app.js

--- CS303/labW1D3/app.js
+++ CS303/labW1D3/app.js
@@ -7,14 +7,35 @@ module.exports = { Accumulator, Calculator }; //add all of your function names h
  * 
  * @returns {Accumulator} constructor function
  */
-function Accumulator(){
-//implement this
+function Accumulator() {
+   //implement this
 
 }
 
+
+
 /**
+* @param {number} num1 a first number
+* @param {number} num2 a second number
  * @returns {Calculator} this is a constructor function
  */
- function Calculator() {
-    //implement this
-     }
+function Calculator() {
+   this.firstNum = 2;
+   this.secondNum = 3;
+   this.setValues = function (num1, num2) {
+      this.firstNum = num1;
+      this.secondNum = num2;
+   };
+   this.sum = function(){
+      return this.firstNum + this.secondNum;
+   };
+   this.mul = function(){
+      return this.firstNum * this.secondNum;
+   };
+}
+
+let calc = new Calculator();
+calc.setValues(20,30);
+console.log(calc.sum());
+console.log(calc.mul());
+console.log(calc.firstNum);
