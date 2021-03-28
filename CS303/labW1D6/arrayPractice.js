b10101f21c1ca727b10b681cb9cac43e75b58880
Yogesh <yghimiray@miu.edu>
2021-03-27 21:34:38 -0500

    Update arrayPractice.js

--- CS303/labW1D6/arrayPractice.js
+++ CS303/labW1D6/arrayPractice.js
@@ -1,37 +1,64 @@
-"use strict"
-/* eslint-disable */
+"use strict";
+
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
 //module.exports = { doubleNums, doubleAges, filterEven, filterOver10, findEvenNum, findEvenAge}; //add all of your function names here that you need for the node mocha tests
 
-
-function doubleNums(arr){
-return arr.map(num => num * 2);
+/**
+ * 
+ * @param {Array} arr an array of numbers
+ * @returns {Array} an array of doubled numbers.
+ */
+function doubleNums(arr) {
+    return arr.map(num => num * 2);
 }
 
-function doubleAges(arr){
+/**
+ * 
+ * @param {Array} arr an array of objects
+ * @returns {Array} an array of objects with doubled ages
+ */
+function doubleAges(arr) {
+    return arr.map(obj => ({
+        name: obj.name,
+        age: obj.age * 2,
+    }));
+}
+
+/**
+ * 
+ * @param {number} num a number
+ * @returns {boolean} either even or not
+ */
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    }
+    return false;
+}
+
+function filterEven(arr) {
+    const even = arr.filter(item => item % 2 === 0);
+    return even;
 
 }
 
-function filterEven(arr){
-
-}
 
 function filterOver10(arr) {
 
 }
 
-function findEvenNum(arr){
+function findEvenNum(arr) {
 
 }
 
-function findEvenAge(arr){
+function findEvenAge(arr) {
 
 }
 
-function includesEvenNum(arr){
+function includesEvenNum(arr) {
 
 }
 
-function includesEvenAge(arr){
+function includesEvenAge(arr) {
 
 }
