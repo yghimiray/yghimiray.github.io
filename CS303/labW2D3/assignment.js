70ffd12baaec1b89e0ef54c06381d9196340cafd
Yogesh <yghimiray@miu.edu>
2021-03-31 21:47:23 -0500

    Update assignment.js

--- CS303/labW2D3/assignment.js
+++ CS303/labW2D3/assignment.js
@@ -18,7 +18,7 @@ function countInstances(str, letter) {
     }
 }
 
-console.log(countInstances("abanana", "a"));
+// console.log(countInstances("abanana", "a"));
 
 
 
@@ -43,11 +43,11 @@ function myMap(arr, cbk) {
 const dblFun = num => num * 2;
 const squareFun = num => num * num;
 
-console.log("expect [2, 4, 6,8,10] ", arr.map(dblFun));
-console.log("expect [2, 4, 6,8,10] ", myMap(arr, dblFun));
+// console.log("expect [2, 4, 6,8,10] ", arr.map(dblFun));
+// console.log("expect [2, 4, 6,8,10] ", myMap(arr, dblFun));
 
-console.log("expect [1, 4, 9, 16, 25] ", arr.map(squareFun));
-console.log("expect [1, 4, 9, 16, 25] ", myMap(arr, squareFun));
+// console.log("expect [1, 4, 9, 16, 25] ", arr.map(squareFun));
+// console.log("expect [1, 4, 9, 16, 25] ", myMap(arr, squareFun));
 
 /**
  * 
@@ -70,11 +70,11 @@ const sumFun = (accum, val) => accum + val;
 const mulFun = (accum, val) => accum * val;
 
 
-console.log("expect: 15: ", arr.reduce(sumFun, 0));
-console.log("expect: 15: ", myReduce(arr, sumFun, 0));
+// console.log("expect: 15: ", arr.reduce(sumFun, 0));
+// console.log("expect: 15: ", myReduce(arr, sumFun, 0));
 
-console.log("expect: 120: ", arr.reduce(mulFun, 1));
-console.log("expect: 120: ", myReduce(arr, mulFun, 1));
+// console.log("expect: 120: ", arr.reduce(mulFun, 1));
+// console.log("expect: 120: ", myReduce(arr, mulFun, 1));
 
 
 /** * 
@@ -97,4 +97,70 @@ function myFind(arr, fun) {
 // }
 
 // const array = [1,2,3,4,5];
-// console.log(myFilter(array,fun));
+// console.log(myFind(array,fun));
+
+const bank = {
+    transactionDB: [],
+};
+
+bank.transactionDB = [
+    { custID: 1, custTrans: [10, 50, -40] }, // balance = 20
+    { custID: 2, custTrans: [10, 10, -10] }, // balance = 10
+    { custID: 3, custTrans: [5, -5, 55] }, // balance = 55
+];
+
+bank.checkId = function (id) {
+    const foundCustmr = bank.transactionDB.find(customer => customer.custID === id);
+    return foundCustmr;
+};
+
+bank.getBalance = function (id) {
+    const customer = this.checkId(id);
+    const balance = customer.custTrans.reduce((sum, item) => sum + item, 0);
+    return balance
+}
+
+bank.saveTransaction = function (id, amount) {
+    const customer = this.checkId(id);
+    customer.custTrans.push(amount);
+}
+
+bank.debit = function (id, amount) {
+    let balance = this.getBalance(id);
+    if (amount < 0) {
+        console.log("Invalid amount. Please enter positive amount !");
+    } else if (amount > balance) {
+        console.log("Insufficient balance. Please try again !");
+    } else {
+        amount = amount * -1;
+        this.saveTransaction(id, amount);
+    }
+}
+
+
+bank.credit = function (id, amount) {
+    if (amount < 0) {
+        console.log("Invalid amount. Please enter positive amount !");
+    } else {
+        this.saveTransaction(id, amount);
+    }
+}
+
+bank.getTotalBalance = function () {
+    const custArr = this.transactionDB;
+    let sum = 0;
+    for (const customer of custArr) {
+        const id = customer.custID;
+        const custBalance = this.getBalance(id);
+        sum += custBalance;
+    }
+    return sum;
+}
+
+console.log(bank.totalBalance());
+
+// bank.debit(2, 10);
+// console.log(bank.transactionDB[1]);
+
+
+
